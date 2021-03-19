07dc7cc1f97c271a3955d57675a132ae68170521
Lucas Ord <ordlucas2332@gmail.com>
2021-03-19 19:05:01 +0000

    Support standing torches, sconce and brazier,  better fuel count check

--- ConfigurableFuel/ConfigurableFire.cs
+++ ConfigurableFuel/ConfigurableFire.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace ConfigurableFire
 {
-    [BepInPlugin("goonlou.ConfigurableFire", "Configurable Fire", "0.1.0")]
+    [BepInPlugin("goonlou.ConfigurableFire", "Configurable Fire", "0.1.1")]
     [BepInProcess("valheim.exe")]
     public class ConfigurableFire : BaseUnityPlugin
     {
@@ -29,23 +29,53 @@ namespace ConfigurableFire
             modEnabled = Config.Bind<bool>("00_General", "Enabled", true, "Enable this mod");
             if (!modEnabled.Value) return;
 
-            Config.Bind<bool>("01_fire_pit", "fire_pit_NoFuel", false, "Allow fire Pit to burn without fuel");
+            Config.Bind<bool>("01_fire_pit", "fire_pit_NoFuel", false, "Allow Fire Pit to burn without fuel");
             Config.Bind<string>("01_fire_pit", "fire_pit_FuelType", "Wood", "Fuel type for Fire Pit. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
             Config.Bind<float>("01_fire_pit", "fire_pit_MaxFuel", 10f, "Maximum fuel level for Fire Pit");
             Config.Bind<float>("01_fire_pit", "fire_pit_StartFuel", 1f, "Start fuel level for Fire Pit");
             Config.Bind<float>("01_fire_pit", "fire_pit_FuelTimeToBurn", 5000f, "Time for Fire Pit to burn 1 fuel (sec)");
 
             Config.Bind<bool>("02_bonfire", "bonfire_NoFuel", false, "Allow Bonfire to burn without fuel");
-            Config.Bind<string>("02_bonfire", "bonfire_FuelType", "Wood", "Fuel type for Bonfire pit. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
-            Config.Bind<float>("02_bonfire", "bonfire_MaxFuel", 10f, "Maximum fuel level for Bonfire pit");
-            Config.Bind<float>("02_bonfire", "bonfire_StartFuel", 0f, "Start fuel level for Bonfire pit");
-            Config.Bind<float>("02_bonfire", "bonfire_FuelTimeToBurn", 5000f, "Time for Bonfire pit to burn 1 fuel (sec)");
+            Config.Bind<string>("02_bonfire", "bonfire_FuelType", "Wood", "Fuel type for Bonfire. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("02_bonfire", "bonfire_MaxFuel", 10f, "Maximum fuel level for Bonfire");
+            Config.Bind<float>("02_bonfire", "bonfire_StartFuel", 0f, "Start fuel level for Bonfire");
+            Config.Bind<float>("02_bonfire", "bonfire_FuelTimeToBurn", 5000f, "Time for Bonfire to burn 1 fuel (sec)");
 
             Config.Bind<bool>("03_hearth", "hearth_NoFuel", false, "Allow Hearth to burn without fuel");
-            Config.Bind<string>("03_hearth", "hearth_FuelType", "Wood", "Fuel type for Hearth pit. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
-            Config.Bind<float>("03_hearth", "hearth_MaxFuel", 20f, "Maximum fuel level for Hearth pit");
-            Config.Bind<float>("03_hearth", "hearth_StartFuel", 0f, "Start fuel level for Hearth pit");
-            Config.Bind<float>("03_hearth", "hearth_FuelTimeToBurn", 5000f, "Time for Hearth pit to burn 1 fuel (sec)");
+            Config.Bind<string>("03_hearth", "hearth_FuelType", "Wood", "Fuel type for Hearth. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("03_hearth", "hearth_MaxFuel", 20f, "Maximum fuel level for Hearth");
+            Config.Bind<float>("03_hearth", "hearth_StartFuel", 0f, "Start fuel level for Hearth");
+            Config.Bind<float>("03_hearth", "hearth_FuelTimeToBurn", 5000f, "Time for Hearth to burn 1 fuel (sec)");
+
+            Config.Bind<bool>("04_wood_torch", "wood_torch_NoFuel", false, "Allow Standing Wood Torch to burn without fuel");
+            Config.Bind<string>("04_wood_torch", "wood_torch_FuelType", "Resin", "Fuel type for Standing Wood Torch. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("04_wood_torch", "wood_torch_MaxFuel", 4f, "Maximum fuel level for Standing Wood Torch");
+            Config.Bind<float>("04_wood_torch", "wood_torch_StartFuel", 2f, "Start fuel level for Standing Wood Torch");
+            Config.Bind<float>("04_wood_torch", "wood_torch_FuelTimeToBurn", 10000f, "Time for Standing Wood Torch to burn 1 fuel (sec)");
+
+            Config.Bind<bool>("05_iron_torch", "iron_torch_NoFuel", false, "Allow Standing Iron Torch to burn without fuel");
+            Config.Bind<string>("05_iron_torch", "iron_torch_FuelType", "Resin", "Fuel type for Standing Iron Torch pit. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("05_iron_torch", "iron_torch_MaxFuel", 6f, "Maximum fuel level for Standing Iron Torch pit");
+            Config.Bind<float>("05_iron_torch", "iron_torch_StartFuel", 2f, "Start fuel level for Standing Iron Torch pit");
+            Config.Bind<float>("05_iron_torch", "iron_torch_FuelTimeToBurn", 20000f, "Time for Standing Iron Torch pit to burn 1 fuel (sec)");
+
+            Config.Bind<bool>("06_green_torch", "green_torch_NoFuel", false, "Allow Standing Green Torch to burn without fuel");
+            Config.Bind<string>("06_green_torch", "green_torch_FuelType", "Guck", "Fuel type for Standing Green Torch. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("06_green_torch", "green_torch_MaxFuel", 6f, "Maximum fuel level for Standing Green Torch");
+            Config.Bind<float>("06_green_torch", "green_torch_StartFuel", 2f, "Start fuel level for Standing Green Torch");
+            Config.Bind<float>("06_green_torch", "green_torch_FuelTimeToBurn", 20000f, "Time for Standing Green Torch to burn 1 fuel (sec)");
+
+            Config.Bind<bool>("07_sconce", "sconce_NoFuel", false, "Allow Sconce to burn without fuel");
+            Config.Bind<string>("07_sconce", "sconce_FuelType", "Resin", "Fuel type for Sconce. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("07_sconce", "sconce_MaxFuel", 6f, "Maximum fuel level for Sconce");
+            Config.Bind<float>("07_sconce", "sconce_StartFuel", 2f, "Start fuel level for Sconce");
+            Config.Bind<float>("07_sconce", "sconce_FuelTimeToBurn", 20000f, "Time for Sconce to burn 1 fuel (sec)");
+
+            Config.Bind<bool>("08_brazier", "brazier_NoFuel", false, "Allow Brazier to burn without fuel");
+            Config.Bind<string>("08_brazier", "brazier_FuelType", "Coal", "Fuel type for Brazier. Swap with any Prefab Name https://github.com/Valheim-Modding/Wiki/wiki/ObjectDB-Table");
+            Config.Bind<float>("08_brazier", "brazier_MaxFuel", 5f, "Maximum fuel level for Brazier");
+            Config.Bind<float>("08_brazier", "brazier_StartFuel", 1f, "Start fuel level for Brazier");
+            Config.Bind<float>("08_brazier", "brazier_FuelTimeToBurn", 20000f, "Time for Brazier to burn 1 fuel (sec)");
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
@@ -82,17 +112,20 @@ namespace ConfigurableFire
                     }
                     else
                     {
-                        bool newState = !configureFuelComponent.GetToggledOn();
-                        configureFuelComponent.SetToggledOn(newState);
-                        if (newState)
+                        bool prevState = configureFuelComponent.GetToggledOn();
+                        bool newState = !prevState;
+
+                        if (!prevState)
                         {
                             configureFuelComponent.gameObject.GetComponent<ZNetView>().GetZDO().Set("fuel", configureFuelComponent.GetCurrentFuel());
-                        }
+                        } 
                         else
                         {
-                            configureFuelComponent.gameObject.GetComponent<ZNetView>().GetZDO().Set("fuel", 0f);
+                            configureFuelComponent.SetCurrentFuel(configureFuelComponent.gameObject.GetComponent<ZNetView>().GetZDO().GetFloat("fuel"));
                         }
-
+                        configureFuelComponent.SetToggledOn(newState);
+                        if (!newState) configureFuelComponent.gameObject.GetComponent<ZNetView>().GetZDO().Set("fuel", 0f);
+                        
                         ((ZSyncAnimation)typeof(Player).GetField("m_zanim", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Player.m_localPlayer)).SetTrigger("interact");
                     }
                     
@@ -126,6 +159,23 @@ namespace ConfigurableFire
                 } else if (__instance.name.Contains("hearth")) {
                     fireName = "hearth";
                     tabName = "03_hearth";
+                } else if (__instance.name.Contains("groundtorch")) {
+                    if (__instance.name.Contains("wood")) {
+                        fireName = "wood_torch";
+                        tabName = "04_wood_torch";
+                    } else if (__instance.name.Contains("green")) {
+                        fireName = "green_torch";
+                        tabName = "06_green_torch";
+                    } else {
+                        fireName = "iron_torch";
+                        tabName = "05_iron_torch";
+                    }
+                } else if (__instance.name.Contains("walltorch")) {
+                    fireName = "sconce";
+                    tabName = "07_sconce";
+                } else if (__instance.name.Contains("brazier")) {
+                    fireName = "brazier";
+                    tabName = "08_brazier";
                 }
 
                 if (fireName != null && fireName != "")
@@ -216,18 +266,21 @@ namespace ConfigurableFire
                             {
                                 if (inventory.HaveItem(__instance.m_fuelItem.m_itemData.m_shared.m_name))
                                 {
-                                    if (configureFuelComponent.GetCurrentFuel() >= __instance.m_maxFuel)
+                                    if (configureFuelComponent.GetCurrentFuel() + 1 >= __instance.m_maxFuel)
                                     {
                                         user.Message(MessageHud.MessageType.Center, Localization.instance.Localize("$msg_cantaddmore", __instance.m_fuelItem.m_itemData.m_shared.m_name));
                                         __result = false;
                                         return false;
                                     }
-                                    user.Message(MessageHud.MessageType.Center, Localization.instance.Localize("$msg_fireadding", __instance.m_fuelItem.m_itemData.m_shared.m_name));
-                                    inventory.RemoveItem(__instance.m_fuelItem.m_itemData.m_shared.m_name, 1);
-                                    ___m_nview.InvokeRPC("AddFuel");
-                                    configureFuelComponent.SetCurrentFuel(configureFuelComponent.GetCurrentFuel() + 1);
-                                    __result = true;
-                                    return false;
+                                    else
+                                    {
+                                        user.Message(MessageHud.MessageType.Center, Localization.instance.Localize("$msg_fireadding", __instance.m_fuelItem.m_itemData.m_shared.m_name));
+                                        inventory.RemoveItem(__instance.m_fuelItem.m_itemData.m_shared.m_name, 1);
+                                        ___m_nview.InvokeRPC("AddFuel");
+                                        configureFuelComponent.SetCurrentFuel(configureFuelComponent.GetCurrentFuel() + 1);
+                                        __result = true;
+                                        return false;
+                                    }
                                 }
                                 user.Message(MessageHud.MessageType.Center, "$msg_outof " + __instance.m_fuelItem.m_itemData.m_shared.m_name);
                                 __result = false;
